9ec7df63aab98a03e2eaabb38cc680ac06e2233d
Anton Ashurek <antonashurek@192.168.1.4>
2023-09-30 13:25:10 +0200

    create basic app layout

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import AppHeader from './app-header/app-header';
+import AppFooter from './app-footer/app-footer';
+import LoginBtn from './login-btn/login-btn';
+
+export { AppHeader, AppFooter, LoginBtn };
