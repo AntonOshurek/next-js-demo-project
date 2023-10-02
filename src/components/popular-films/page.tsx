2ef1fd13289f59462fcca9ccd0e5953345b4fad6
Anton Ashurek <antonashurek@192.168.1.4>
2023-10-02 20:02:23 +0200

    add editorconfig

--- src/components/popular-films/page.tsx
+++ src/components/popular-films/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 /* API */
 import filmsApi from '@/api/films-api';
 /* STYLES */
@@ -6,35 +7,38 @@ import style from './style.module.css';
 import type IPopular from '@/types/movie/popular';
 
 const PopularFilms = async () => {
-    const popularFilms: IPopular = await filmsApi.moviePopular('eng', 1);
+	const popularFilms: IPopular = await filmsApi.moviePopular('eng', 1);
 
-    return (
-        <section className={style['popular-films']}>
-            <div className={style['popular-films__wrap']}>
-                <h2 className={style['popular-films__title']}>Popular movies</h2>
+	return (
+		<section className={style['popular-films']}>
+			<div className={style['popular-films__wrap']}>
+				<h2 className={style['popular-films__title']}>Popular movies</h2>
 
 
-                <div className={style['popular-films__slider']}>
-                    <ul className={style['popular-films__list']}>
-                        {
-                            popularFilms.results.map((movie) => {
-                                return (
-                                    <li className={style['popular-films__item']}>
-                                        <div className={style['popular-films__item-info']}>
-                                            <h3 className={style['popular-films__item-title']}>{movie.title}</h3>
-                                            {/* <p className={style['popular-films__item-overvie']}>{movie.overview}</p> */}
-                                        </div>
-                                        <img className={style['popular-films__item-poster']} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} width={200} height={200} alt="" />
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-            </div>
+				<div className={style['popular-films__slider']}>
+					<ul className={style['popular-films__list']}>
+						{
+							popularFilms.results.map((movie) => {
+								return (
+									<li className={style['popular-films__item']} key={movie.id}>
+										<div className={style['popular-films__item-info']}>
+											<h3 className={style['popular-films__item-title']}>{movie.title}</h3>
+											{/* <p className={style['popular-films__item-overvie']}>{movie.overview}</p> */}
+										</div>
+										<img className={style['popular-films__item-poster']} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} width={200} height={200} alt="" />
+										<Link href={`/movie/${movie.id}`}>
+											<span className='visually-hidden'>link to ${movie.original_title} movie</span>
+										</Link>
+									</li>
+								)
+							})
+						}
+					</ul>
+				</div>
+			</div>
 
-        </section>
-    );
+		</section>
+	);
 };
 
 export default PopularFilms;
