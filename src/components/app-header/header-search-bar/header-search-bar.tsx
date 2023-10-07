e153eba23cb58a0b1a1d7253dfb6681c9f1f0cb7
Anton Ashurek <antonashurek@192.168.1.4>
2023-10-07 19:32:34 +0200

    search correction

--- src/components/app-header/header-search-bar/header-search-bar.tsx
+++ src/components/app-header/header-search-bar/header-search-bar.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { ChangeEvent, useEffect, useState } from 'react';
 import filmsApi from '@/api/films-api';
-import type { ISearchMulti } from '@/types/movie/search';
+import type { IMovieInSearch } from '@/types/movie/search';
 /* styles */
 import style from './header-search-bar.module.css';
 import Link from 'next/link';
 
 const HeaderSearchBar = () => {
 	const [searchText, setSearchText] = useState<string>('');
-	const [searchResult, setSearchResult] = useState<ISearchMulti | null>(null);
+	const [searchResult, setSearchResult] = useState<IMovieInSearch[] | null>(null);
 
 	const searchInputHandler = (evt: ChangeEvent<HTMLInputElement>) => {
 		setSearchText(evt.target.value);
@@ -18,13 +18,23 @@ const HeaderSearchBar = () => {
 		if (searchText.length > 0) {
 			filmsApi.searchMulti(searchText)
 				.then((res) => {
-					setSearchResult(res);
+					if (res?.results) {
+						setSearchResult(res.results);
+					} else {
+						setSearchResult(null);
+					}
 				})
 				.catch((err) => {
 					console.log(err)
 				})
+		} else {
+			setSearchResult(null);
 		}
 	}, [searchText]);
+
+	const linkHandler = () => {
+		setSearchResult(null);
+	}
 
 	return (
 		<div className={`${style['header-search-bar']}`}>
@@ -41,18 +51,22 @@ const HeaderSearchBar = () => {
 
 			{
 				searchResult && <ul className={`${style['header-search-bar__search-list']}`}>
-					{searchResult.results.map((item, i) => {
-						if (i < 4) {
-							return (
-								<li className={`${style['header-search-bar__search-list-item']}`} key={item.id}>
-									<Link className={`${style['header-search-bar__search-list-link']}`} href={`/movie/${item.id}`}>
-										{item.title}
-									</Link>
-								</li>
-							)
-						}
-						return null;
-					})}
+
+					{
+						searchResult.length > 0 ? searchResult.map((item, i) => {
+							if (i < 4) {
+								return (
+									<li className={`${style['header-search-bar__search-list-item']}`} key={item.id}>
+										<Link className={`${style['header-search-bar__search-list-link']}`} href={`/movie/${item.id}`}
+											onClick={linkHandler}>
+											{item.title}
+										</Link>
+									</li>
+								)
+							}
+							return null;
+						}) : <p className={`${style['header-search-bar__search-without-result']}`}>Ничего не найдено :(</p>
+					}
 				</ul>
 			}
 		</div>
